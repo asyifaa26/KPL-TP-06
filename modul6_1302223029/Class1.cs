0c26731e73e2d2c68234a1e247ec28c841997367
asyifaa26 <153969653+asyifaa26@users.noreply.github.com>
2024-03-24 23:51:38 +0700

    menambahkan class SayaTubeVideo

---
+++ modul6_1302223029/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302223029
+{
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            
+            Random acak = new Random();
+            id = acak.Next(9999, 100001);
+
+            
+            Debug.Assert(title.Length <= 100 && title != null);
+            this.title = title;
+
+            
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+
+        }
+
+        
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"Video ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
--- modul6_1302223029/obj/Debug/net8.0/modul6_1302223029.AssemblyInfo.cs
+++ modul6_1302223029/obj/Debug/net8.0/modul6_1302223029.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul6_1302223029")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb45a02c442f287eab17ecb317eed7560f2e8ceb")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul6_1302223029")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul6_1302223029")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
